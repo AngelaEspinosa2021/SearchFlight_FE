5058aa190db13e662c3f7e7e439fee40fadb107e
AngelaEspinosa2021 <angelaespinosa23871@ucaldas.edu.co>
2021-11-28 17:10:47 -0500

    Importación del FlightService dentro del core.module.ts

--- SearchFlightFE/src/app/core/core.module.ts
+++ SearchFlightFE/src/app/core/core.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { FlightService } from './services/flights/flight.service';
+
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule
+  ],
+  providers: [
+    FlightService
   ]
 })
 export class CoreModule { }
