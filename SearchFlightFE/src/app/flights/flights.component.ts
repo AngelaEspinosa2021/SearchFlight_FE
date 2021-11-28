9fdb7655ef01dea6bf3212f5e1ea462233f8ee1b
AngelaEspinosa2021 <angelaespinosa23871@ucaldas.edu.co>
2021-11-28 17:03:28 -0500

    Importación del SharedModule dentro de FlightsComponent

--- SearchFlightFE/src/app/flights/flights.component.ts
+++ SearchFlightFE/src/app/flights/flights.component.ts
@@ -1,10 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgModule, OnInit } from '@angular/core';
+import { SharedModule } from '../shared/shared.module';
 
 @Component({
   selector: 'app-flights',
   templateUrl: './flights.component.html',
   styleUrls: ['./flights.component.css']
 })
+
+@NgModule({
+  imports: [
+    SharedModule
+  ]
+})
+
 export class FlightsComponent implements OnInit {
 
   constructor() { }
